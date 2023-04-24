7c034c4f28fd22590993bf1e5f89d5ae15d034ad
JoannaCodes <joannalara21@gmail.com>
2023-04-24 16:23:50 +0800

    jlara update: added query functions

--- index.php
+++ index.php
@@ -7,55 +7,71 @@ include_once "mod/queries.php";
 
 
 
-if(isset($_POST["tag"])){
-//POST
-$tag = $_POST["tag"];
-}else if(isset($_GET["tag"])){
-//GET
-$tag = $_GET["tag"];
+if (isset($_POST["tag"])) {
+	//POST
+	$tag = $_POST["tag"];
+} else if (isset($_GET["tag"])) {
+	//GET
+	$tag = $_GET["tag"];
 }
 
 switch ($tag) {
 	case 'upload_image':
-  		$document = $_FILES['document'];
-  		$uploadDir = './uploads/';
-	    $uploadFile = $uploadDir . basename($document['name']);
-	    $fileName = basename($document['name']);
+		$document = $_FILES['document'];
+		$uploadDir = './uploads/';
+		$uploadFile = $uploadDir . basename($document['name']);
+		$fileName = basename($document['name']);
 
-	    $out = sdmq()->send_document($fileName);
-	    if($out == "1") {
-	    	if (move_uploaded_file($document['tmp_name'], $uploadFile)) {
-		      echo 'File is valid, and was successfully uploaded.';
-		    } else {
-		      echo 'Upload failed.';
-		    }
-	    } else {
-	    	echo "Query failed.";
-	    }
-	break;
+		$out = sdmq()->send_document($fileName);
+		if ($out == "1") {
+			if (move_uploaded_file($document['tmp_name'], $uploadFile)) {
+				echo 'File is valid, and was successfully uploaded.';
+			} else {
+				echo 'Upload failed.';
+			}
+		} else {
+			echo "Query failed.";
+		}
+		break;
 	case 'login':
-		echo sdmq()->login_dormfinder($_POST["email"],$_POST["password"]);
-	break;
+		echo sdmq()->login_dormfinder($_POST["email"], $_POST["password"]);
+		break;
 	case 'verify_document':
-		echo sdmq()->verify_document($_POST['id'],$_POST['docvalue']);
-	break;
+		echo sdmq()->verify_document($_POST['id'], $_POST['docvalue']);
+		break;
 	case 'fetch_saved_notif':
 		echo sdmq()->look_usersavednotifs();
-	break;
+		break;
 	case 'get_submitdocuments':
 		echo sdmq()->get_submitdocuments();
-	break;
+		break;
 	case "getmorenotification":
-		echo sdmq()->look_morepastnotif($_GET["userref"],$_GET["idstoftech"]);
-	break;
+		echo sdmq()->look_morepastnotif($_GET["userref"], $_GET["idstoftech"]);
+		break;
 	case "getnotificationspast":
 		echo sdmq()->look_passednotifications($_GET["userref"]);
-	break;
+		break;
+	case "update_account":
+		$out = sdmq()->update_account($_POST["userref"], $_POST["loginCredential"]);
+		if ($out == "1") {
+			echo 'Account Updated';
+		} else {
+			echo 'Cannot Update Account. Please Try Again';
+		}
+		break;
+	case 'delete_account':
+		$out = sdmq()->delete_account($_POST["userref"]);
+		if ($out == "1") {
+			echo 'Account Deleted';
+		} else {
+			echo 'Cannot Delete Account. Pleass Try Again';
+		}
+		break;
 }
-function sdmq(){
+function sdmq()
+{
 	$c = new connection();
 	$c = $c->sdm_connect();
 	$sdm_q = new sdm_query($c);
 	return $sdm_q;
 }
-?>
